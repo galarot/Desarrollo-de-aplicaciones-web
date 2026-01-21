a06ef28ed053f9484e883b0276677a85b7d917c4
Manu <mmd0012@alu.medac.es>
2026-01-21 14:24:17 +0100

    tablero 21 ene

--- Desarrollo entorno cliente/TableroKanban/main.js
+++ Desarrollo entorno cliente/TableroKanban/main.js
@@ -1,6 +1,9 @@
 const inputNum = document.getElementById("num");
 const contenido = document.getElementById("tabla");
 const form = document.getElementById("formulario");
+const cols = document.getElementById("columnas");
+const configSec = document.getElementById("configSec")
+const tabSec = document.getElementById("tabSec")
 
 
 function obtener (){
@@ -16,6 +19,30 @@ function guardar(arrayDatos){
     localStorage.setItem("tablero", JSON.stringify(arrayDatos));
 }
 
+inputNum.addEventListener("input", () =>{
+    cols.innerHTML = "";
+    const cant = parseInt(inputNum.value);
+
+    for(let i = 0; i < cantidad; i++){
+        const titulo = document.createElement("h4");
+        titulo.textContent.Content = "Configurar columna: " + (i + 1);
+
+        const inputNombre = document.createElement("input");
+        inputNombre.type = "text";
+        inputNombre.placeholder = "Nombre";
+        inputNombre.className = "nombreCol";
+
+        const inputMax = document.createElement("input");
+        inputMax.type = "number";
+        inputMax.placeholder = "Limite de tareas"
+        inputMax.className = "limiteCol";
+
+        cols.appendChild(titulo);
+        cols.appendChild(inputNombre);
+        cols.appendChild(inputMax);
+    }
+})
+
 form.addEventListener("submit", (e) =>{
     e.preventDefault();
     const cant = parseInt(inputNum.value);
@@ -30,6 +57,19 @@ form.addEventListener("submit", (e) =>{
 })
 
 function mostrarTabla(){
+    const datos = obtener();
+
+    configSec.style.display = "none";
+    tabSec.style.display = "block";
+    contenido.innerHTML = "";
+
+    datos.forEach((columna, indiceCol) =>{
+        const div = document.createElement("div");
+        div.className = "columna";
+        
+
+    })
+
     contenido.innerHTML= "";
     const tablaActivo = obtener();
 
@@ -37,6 +77,7 @@ function mostrarTabla(){
         const tab = document.createElement("tablero");
         tab.textContent = texto;
 
+        
         
     })
 }
