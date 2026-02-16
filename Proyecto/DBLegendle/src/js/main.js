23f40e74e9f0ac574ade982994f6e50786f593b6
Galarot <mmd0012@alu.medac.es>
2026-02-16 07:12:48 +0100

    porsiaca

--- Proyecto/DBLegendle/src/js/main.js
+++ Proyecto/DBLegendle/src/js/main.js
@@ -10,7 +10,7 @@ let pruebaDia = {};
 
 function seleccionasPerso(){
     const idMin = 1;
-    const idMax = 264;
+    const idMax = 281;
     //temporal ya que algunos en la base de datos no estan definidos del tdo
     const rango = personajes.filter(p => p.id >= idMin && p.id <= idMax);
 
